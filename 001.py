46761aac7c9d4f5e4ffee87433b1287a37f0aa54
manoj-kumar-g-m <159249514+manoj-kumar-g-m@users.noreply.github.com>
2024-02-07 23:54:37 +0530

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,14 +1,3 @@
-"""
-Task
-Given an integer,n , perform the following conditional actions:
-
-If n  is odd, print Weird
-If n is even and in the inclusive range of 2 to 6, print Not Weird
-If n is even and in the inclusive range of 6 to 20, print Weird
-If n is even and greater than 20 , print Not Weird
-
-"""
-
 n = int(input())
 
 if n % 2 != 0:
